f714e471d2dd16e565ea6e53c3b3cd06034e03f4
tendsT0Zero <nayeemofficial80@gmail.com>
2025-10-19 10:52:19 +0600

    How to declare a string type data

--- Introduction to C#/Program.cs
+++ Introduction to C#/Program.cs
@@ -2,4 +2,9 @@
 var userInput = Console.ReadLine();
 
 // default data type when we take input from user:
-Console.WriteLine("Default data type of user input is: " + userInput.GetType());
+Console.WriteLine("Default data type of user input is: " + userInput.GetType()); // Output: System.String
+
+// Working with data type string:
+string stringData=" this is an introduction to C#, say hello world using c#";
+
+// Operations on string data type using Library functions:
